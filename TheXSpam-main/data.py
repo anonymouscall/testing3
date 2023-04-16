07eed6937be1898c6e3f0b711cc7c6e37e1c16d3
anonymouscall <127658425+anonymouscall@users.noreply.github.com>
2023-04-17 04:28:06 +0530

    Add files via upload

---
+++ TheXSpam-main/data.py
@@ -0,0 +1,88 @@
+RAID = [
+   "JESUS DICK SUCKERRR🤣🤣🤣",
+    "JESUS IS MY SPOLIED KID 🤣🤣🤣",
+    "𝗝𝗘𝗦𝗨𝗦 𝗦𝗨𝗖𝗞𝗦 𝗠𝗬 𝗛𝗜𝗡𝗗𝗨 𝗗𝗜𝗖𝗞 🥵🥵🤣🤣🤣🤣🍆🍆🍆",
+    "𝗝𝗘𝗦𝗨𝗦 𝗞𝗜 𝗠𝗔𝗔 𝗞𝗜 𝗖𝗛𝗢𝗢𝗧 𝗠𝗘 𝗠𝗘𝗥𝗔 𝟭𝟬𝗜𝗡𝗖𝗛 𝗞𝗔 𝗟𝗢𝗗𝗔 🍆🍆🤣🤣🤣🤣🤣🤣🤣",
+    "𝗝𝗘𝗦𝗨𝗦 𝗦𝗨𝗖𝗞𝗦 𝗠𝗬 𝗕𝗜𝗚 𝗗𝗜𝗖𝗞 🤣🤣 𝗝𝗘𝗦𝗨𝗦 𝗟𝗢𝗩𝗘𝗦 𝗠𝗬 𝗗𝗜𝗖𝗞𝗞 𝗦𝗢 𝗠𝗨𝗖𝗛 🤣🤣🤣 𝗝𝗘𝗦𝗨𝗦 𝗙𝗨𝗖𝗞𝗦𝗦𝗦 𝗚𝗢𝗔𝗧𝗦 🤣🤣🤣🤣🥵🥵🍆🍆🍆",
+    "𝗝𝗘𝗦𝗨𝗦 𝗗𝗜𝗖𝗞𝗞𝗞 𝗦𝗨𝗖𝗞𝗘𝗥𝗥𝗥🍆🤣🥵🤣🥵🍆 𝗔𝗟𝗟 𝗖𝗛𝗥𝗜𝗦𝗧𝗜𝗔𝗡𝗦 𝗔𝗥𝗘 𝗠𝗬 𝗗𝗜𝗖𝗞𝗞 𝗦𝗨𝗖𝗞𝗘𝗥𝗦𝗦𝗦 🤣🤣🥵🥵🍆🍆🍆𝗖𝗛𝗥𝗜𝗦𝗧𝗜𝗔𝗡𝗦 𝗟𝗢𝗩𝗘 𝗠𝗬 𝗛𝗜𝗡𝗗𝗨𝗨 𝗗𝗜𝗖𝗞𝗞𝗞 𝗦𝗢𝗢𝗢 𝗞𝗨𝗖𝗛𝗛 🥵🤣🤣🍆🍆🍆🤣🍆🥵",
+    "𝗜 𝗙𝗨𝗖𝗞𝗘𝗗𝗗 𝗝𝗘𝗦𝗨𝗦 𝗦𝗢 𝗛𝗔𝗥𝗗...🥵
+𝗝𝗘𝗦𝗨𝗦 𝗦𝗧𝗔𝗥𝗧 𝗧𝗢 𝗖𝗥𝗬 😭🤣🤣🍆🥵🥵𝗜 𝗙𝗨𝗖𝗞𝗞 𝗔𝗟𝗟 𝗖𝗛𝗥𝗜𝗦𝗧𝗜𝗔𝗡𝗦 🤣🥵🍆🥵🥵🥵
+☪️𝗛𝗨✝️𝗜𝗬𝗔",
+    "𝗝𝗘𝗦𝗨𝗦 𝗠𝗔𝗗𝗔𝗥𝗖𝗛𝗢𝗢𝗗 𝗥𝗔𝗡𝗗𝗜𝗜𝗜 𝗞𝗔 𝗕𝗔𝗖𝗖𝗛𝗔🤣🤣🤣🤣🤣𝗦𝗔𝗕 𝗝𝗘𝗦𝗨𝗦 𝗞𝗜 𝗖𝗛𝗨𝗗𝗔𝗜 𝗗𝗘𝗞𝗛𝗢 𝗫𝗫𝗫.𝗖𝗢𝗠 𝗣𝗘 🤣🍆🥵🤣🤣🤣🤣",
+    "𝗝𝗘𝗦𝗨𝗦 𝗠𝗔𝗗𝗔𝗥𝗖𝗛𝗢𝗢𝗗 𝗥𝗔𝗡𝗗𝗜𝗜𝗜 𝗞𝗔 𝗕𝗔𝗖𝗖𝗛𝗔🤣🤣🤣🤣🤣𝗦𝗔𝗕 𝗝𝗘𝗦𝗨𝗦 𝗞𝗜 𝗖𝗛𝗨𝗗𝗔𝗜 𝗗𝗘𝗞𝗛𝗢 𝗫𝗫𝗫.𝗖𝗢𝗠 𝗣𝗘 🤣🍆🥵🤣🤣🤣🤣",
+    "𝗝𝗘𝗦𝗨𝗦 𝗞𝗜 𝗠𝗔𝗔𝗞𝗜 𝗖𝗛𝗢𝗢𝗧 𝗠𝗘 𝗧𝗛𝗔𝗣𝗣𝗔𝗗 MAARA🥵🥵🍆🤣🤣🤣🤣
+𝗝𝗘𝗦𝗨𝗦 𝗞𝗜 𝗔𝗠𝗠𝗜 𝗞𝗜 𝗖𝗛𝗢𝗢𝗢𝗧 𝗙𝗔𝗧𝗧 𝗚𝗬𝗜 🤣🤣🤣🤣🤣🤣🤣🤣",
+    "𝗜 𝗙𝗨𝗫𝗞𝗞 𝗔𝗟𝗟 𝗖𝗛𝗥𝗜𝗦𝗧𝗜𝗔𝗡𝗦 🥵🥵🍆🍆𝗔𝗟𝗟 𝗖𝗛𝗥𝗜𝗦𝗧𝗜𝗔𝗡𝗦 𝗔𝗥𝗘 𝗠𝗬 𝗣𝗘𝗧𝗦 𝗔𝗡𝗗 𝗔𝗟𝗟 𝗖𝗛𝗥𝗜𝗦𝗧𝗜𝗔𝗡𝗦 𝗚𝗜𝗥𝗟𝗦 𝗔𝗥𝗘 𝗪𝗛𝗢𝗥𝗘𝗦 🥵🤣🤣🥵🤣🤣🤣",
+    "𝗬𝗢𝗨 𝗔𝗥𝗘 𝗠𝗬 𝟵𝟴𝗧𝗛 𝗞𝗜𝗗 𝗙𝗥𝗢𝗠 𝗠𝗬 𝟵𝟴𝗧𝗛 𝗪𝗜𝗙𝗘 𝗗𝗜𝗖𝗞𝗞𝗞 𝗦𝗨𝗖𝗞𝗘𝗥𝗙𝗥 🤣🤣🥵🥵🍆🍆𝗚𝗢 𝗔𝗦𝗞 𝗨𝗥 𝗠𝗨𝗠 𝗛𝗢𝗪 𝗛𝗔𝗥𝗗 𝗜 𝗙𝗨𝗖𝗞𝗞𝗞 𝗛𝗘𝗥𝗥 🥵🥵🍆🍆🍆🍆",
+    "𝗜 𝗙𝗨𝗖𝗞𝗘𝗗𝗗 𝗨𝗥 𝗠𝗢𝗠 𝗔𝗡𝗗 𝗝𝗘𝗦𝗨𝗦 𝗧𝗢𝗡𝗜𝗚𝗛𝗧 🥵🥵🍆🍆🍆🍆𝗨𝗥 𝗠𝗢𝗠 𝗦𝗨𝗖𝗞𝗦 𝗠𝗬 𝗗𝗜𝗖𝗞𝗞 𝗡𝗜𝗖𝗘𝗟𝗬 𝗦𝗘𝗡𝗗 𝗛𝗘𝗥 𝗗𝗔𝗜𝗟𝗬 𝗧𝗢 𝗠𝗬 𝗣𝗟𝗔𝗖𝗘 🥵🥵🍆🍆🍆",
+    "𝗝𝗘𝗦𝗨𝗦 𝗦𝗨𝗫𝗞𝗦𝗦 𝗠𝗬 𝗗𝗜𝗖𝗞𝗞 𝗗𝗔𝗬 𝗔𝗡𝗗 𝗡𝗜𝗚𝗛𝗧 🤣🥵🥵🥵🤣🍆🍆🍆🍆🍆
+𝗝𝗘𝗦𝗨𝗦 𝗜𝗦 𝗔 𝗥𝗘𝗔𝗟𝗟𝗬 𝗡𝗜𝗖𝗘 𝗗𝗜𝗖𝗞𝗞𝗞 𝗦𝗨𝗖𝗞𝗘𝗥𝗥 🤣🤣🤣🤣🤣🤣🥵🥵🥵🥵🥵",
+    "𝗜 𝗙𝗨𝗖𝗞𝗞𝗘𝗗 𝗨𝗥 𝗠𝗢𝗠 𝗦𝗢𝗢 𝗛𝗔𝗥𝗗 𝗧𝗛𝗔𝗧 𝗛𝗘𝗥 𝗣𝗨𝗦𝗦𝗬 𝗪𝗘𝗡𝗧 𝗡𝗨𝗠𝗕 🥵🥵🥵🥵🥵🥵🍆🍆🍆🍆 𝗨𝗥 𝗠𝗢𝗠 𝗟𝗢𝗩𝗘𝗦 𝗠𝗬 𝗕𝗜𝗚 𝗗𝗜𝗖𝗞𝗞𝗞 𝗦𝗢𝗢 𝗞𝗨𝗖𝗛𝗛 🥵🥵🥵🥵",
+    "𝗨𝗥 𝗠𝗢𝗠 𝗦𝗨𝗖𝗞𝗦 𝗠𝗬 𝗗𝗜𝗖𝗞 𝗟𝗜𝗞𝗘 𝗨 𝗦𝗨𝗖𝗞𝗘𝗗 𝗠𝗜𝗡𝗘 𝗬𝗘𝗦𝗧𝗘𝗥𝗗𝗔𝗬 🥵🥵🥵🤣🍆🍆🍆🍆🤣🤣🤣🤣🤣🤣",
+    "𝗝𝗘𝗦𝗨𝗦 𝗥𝗔𝗡𝗗𝗜𝗜𝗜 𝗞𝗔 BACCHAA🤣🤣🤣🤣🤣🤣 𝗚𝗔𝗬𝗬𝗬 𝗝𝗘𝗦𝗨𝗦 🍆🥵🤣🤣🤣🤣",
+    "𝗜 𝗙𝗨𝗖𝗞𝗘𝗗𝗗𝗗 𝗨𝗥 𝗠𝗢𝗠 𝗛𝗔𝗥𝗗 🥵🥵𝗝𝗘𝗦𝗨𝗦 𝗦𝗨𝗖𝗞𝗦𝗦 𝗠𝗬 𝗛𝗜𝗡𝗗𝗨𝗨 𝗗𝗜𝗖𝗞𝗞𝗞 🥵🥵🍆🤣🤣🤣",]
+
+
+THE_ALTS = [5518687442, 5306162870]
+GROUP = [-1001313291319, -1001777776331, -1001859846702, "TheAltron", "AltronChats", "@TheAltron", "@AltronChats"]
+
+
+PORM = [
+        "https://telegra.ph/file/9bcc076fd81dfe3feb291.mp4",
+        "https://telegra.ph/file/b7a1a42429a65f64e67af.mp4",
+        "https://telegra.ph/file/dc3da5a3eb77ae20fa21d.mp4",
+        "https://telegra.ph/file/7b15fbca08ae1e73e559c.mp4",
+        "https://telegra.ph/file/a9c1dea3f34925bb60686.mp4",
+        "https://telegra.ph/file/913b4e567b7f435b7f0db.mp4",
+        "https://telegra.ph/file/5a5d1a919a97af2314955.mp4",
+        "https://telegra.ph/file/0f8b903669600d304cbe4.mp4",
+        "https://telegra.ph/file/f3816b54c9eb7617356b6.mp4",
+        "https://telegra.ph/file/516dbaa03fde1aaa70633.mp4",
+        "https://telegra.ph/file/07bba6ead0f1e381b1bd1.mp4",
+        "https://telegra.ph/file/0a4f7935df9b4ab8d62ed.mp4",
+        "https://telegra.ph/file/40966bf68c0e4dbe18058.mp4",
+        "https://telegra.ph/file/50637aa9c04d136687523.mp4",
+        "https://telegra.ph/file/b81c0b0e491da73e64260.mp4",
+        "https://telegra.ph/file/4ddf5f29783d92ae03804.mp4",
+        "https://telegra.ph/file/4037dc2517b702cc208b1.mp4",
+        "https://telegra.ph/file/33cebe2798c15d52a2547.mp4",
+        "https://telegra.ph/file/4dc3c8b03616da516104a.mp4",
+        "https://telegra.ph/file/6b148dace4d987fae8f3e.mp4",
+        "https://telegra.ph/file/8cb081db4eeed88767635.mp4",
+        "https://telegra.ph/file/98d3eb94e6f00ed56ef91.mp4",
+        "https://telegra.ph/file/1fb387cf99e057b62d75d.mp4",
+        "https://telegra.ph/file/6e1161f63879c07a1f213.mp4",
+        "https://telegra.ph/file/0bf4defb9540d2fa6d277.mp4",
+        "https://telegra.ph/file/d5f8280754d9aa5dffa6a.mp4",
+        "https://telegra.ph/file/0f23807ed1930704e2bef.jpg",
+        "https://telegra.ph/file/c49280b8f1dcecaf86c00.jpg",
+        "https://telegra.ph/file/f483400ff141de73767ca.jpg",
+        "https://telegra.ph/file/1543bbea4e3c1abb6764a.jpg",
+        "https://telegra.ph/file/a0d77be0d769c7cd334ab.jpg",
+        "https://telegra.ph/file/6c6e93860527d2f577df8.jpg",
+        "https://telegra.ph/file/d987b0e72eb3bb4801f01.jpg",
+        "https://telegra.ph/file/b434999287d3580250960.jpg",
+        "https://telegra.ph/file/0729cc082bf97347988f7.jpg",
+        "https://telegra.ph/file/bb96d25df82178a2892e7.jpg",
+        "https://telegra.ph/file/be73515791ea33be92a7d.jpg",
+        "https://telegra.ph/file/fe234d6273093282d2dcc.jpg",
+        "https://telegra.ph/file/66254bb72aa8094d38250.jpg",
+        "https://telegra.ph/file/44bdaf37e5f7bdfc53ac6.jpg",
+        "https://telegra.ph/file/e561ee1e1ca88db7e8038.jpg",
+        "https://telegra.ph/file/f1960ccfc866b29ea5ad2.jpg",
+        "https://telegra.ph/file/97622cad291472fb3c4aa.jpg",
+        "https://telegra.ph/file/a46e316b413e9dc43e91b.jpg",
+        "https://telegra.ph/file/497580fc3bddc21e0e162.jpg",
+        "https://telegra.ph/file/3e86cc6cab06a6e2bde82.jpg",
+        "https://telegra.ph/file/83140e2c57ddd95f310e6.jpg",
+        "https://telegra.ph/file/2b20f8509d9437e94fed5.jpg",
+        "https://telegra.ph/file/571960dcee4fce56698a4.jpg",
+        "https://telegra.ph/file/25929a0b49452d8946c14.mp4",
+        "https://telegra.ph/file/f5c9ceded3ee6e76a5931.jpg",
+        "https://telegra.ph/file/a8bf6c6df8a48e4a306ca.jpg",
+        "https://telegra.ph/file/af9e3f98da0bd937adf6e.jpg",
+        "https://telegra.ph/file/2fcccbc72c57b6892d23a.jpg",
+        "https://telegra.ph/file/843109296a90b8a6c5f68.jpg",
+]
+
+OneWord = [ "JESUS MADARCHODD", "JESUS SUCKS DICKK", "JESUS RANDII", "JESUS GANDU", "JESUS GANDU CHUTIYA", "JESUS FATI CHOOT KA", ,"BAAP","SEE","BAKC]
